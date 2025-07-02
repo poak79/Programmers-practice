12baa33865439bcb2ef174138174be6c9daeeb59
박윤서 <parkyunseo5@gmail.com>
2025-07-02 22:46:42 +0900

    문제 4: 대소문자 바꿔서 출력하기

---
+++ Day1/printf_4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define LEN_INPUT 20
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    
+    for (int i=0;i<LEN_INPUT;i++) {
+        if (s1[i] >='a' && s1[i] <='z')  {//97~122
+            s1[i] -= 32;
+        }
+        else if (s1[i] >='A' && s1[i] <='Z') {//65~90
+            s1[i] += 32;
+        }
+    }
+    
+    printf("%s", s1);
+    return 0;
+}
