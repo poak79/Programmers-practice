4aa2c4f62cf055256813361637000e0dc2a4480c
poak79 <parkyunseo5@gmail.com>
2026-06-12 00:31:10 +0900

    [level 0] Title: 코드 처리하기, Time: 6.31 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,19 @@
+def solution(code):
+    ret = ''
+    mode = 0
+    for idx in range(len(code)):
+        if mode==0:
+            if code[idx]!='1':
+                if idx%2==0:
+                    ret += (code[idx])
+            else:
+                mode=1
+        elif mode==1:
+            if code[idx]!='1':
+                if idx%2!=0:
+                    ret += (code[idx])
+            else:
+                mode=0
+            
+    return "EMPTY" if ret == "" else ret
+        
