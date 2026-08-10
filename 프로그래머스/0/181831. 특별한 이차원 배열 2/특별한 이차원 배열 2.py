778b03f4dff7f9dc27d7030138939cfbb66d5429
poak79 <parkyunseo5@gmail.com>
2026-08-10 20:55:49 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.48 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -0,0 +1,7 @@
+def solution(arr):
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)):
+            if arr[i][j] != arr[j][i]:
+                return 0
+            
+    return 1
